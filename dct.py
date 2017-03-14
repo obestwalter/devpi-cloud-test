0bc11fda27e3260ffdc7328bf9199e7864dc510d
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-03-14 21:54:51 +0100

    naming things for toy projects is fun :D

--- dct.py
+++ dct.py
@@ -115,7 +115,7 @@ class Memory:
         which.write(project + "\n" + version, encoding='utf-8')
 
 
-def with_memory(func):
+def read_only_memory(func):
     def _with_memory(self, *args, **kwargs):
         if not hasattr(self, 'runner'):
             log.warning("nothing to work with. Call 'dct set' to get started")
@@ -139,22 +139,22 @@ class Cli():
         if [project, version] != Memory.get():
             self.runner = Runner(*Memory.get())
 
-    @with_memory
+    @read_only_memory
     def test(self):
         """Trigger CI tests for configured build"""
         self.runner.trigger_tests()
 
-    @with_memory
+    @read_only_memory
     def release(self):
         """Release the current build (still a NOP)"""
         self.runner.release()
 
-    @with_memory
+    @read_only_memory
     def retract(self):
         """Release the current build (still a NOP)"""
         self.runner.retract()
 
-    @with_memory
+    @read_only_memory
     def render_only(self):
         """Update files from templates only"""
         self.runner.render_files()
