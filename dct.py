99e11f7b7f1d8f21cc0c4612b154f02e781cc2bb
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-03-16 20:39:41 +0100

    * use normal logging * make more fine grained access possible * do sanity check in decorator

--- dct.py
+++ dct.py
@@ -1,18 +1,38 @@
+import logging
 from configparser import ConfigParser
 from datetime import datetime
 from string import Template
 
-import devpy
 import fire
 from plumbum import local, LocalPath
 
-log = devpy.autolog(level='DEBUG', log_filename=False)
+log = logging.getLogger(__name__)
+
+
+def with_sanity(func):
+    def _with_sanity(*args, **kwargs):
+        if not LocalPath(Dct._iniName).exists():
+            log.error("no %s in '%s'" % (Dct._iniName, local.cwd))
+            exit(1)
+
+        return func(*args, **kwargs)
+    return _with_sanity
 
 
 class Dct:
+    """Normal usage:
+
+    dct trigger <version>
+
+    Advanced usage see:
+    https://github.com/google/python-fire/blob/master/doc/using-cli.md
+    """
+    _iniName = 'dct.ini'
+
+    @with_sanity
     def __init__(self):
         config = ConfigParser()
-        config.read('dct.ini')
+        config.read(self._iniName)
         dct = config['dct']
         self._package = dct['package']
         self._devpiUser = dct['devpi_user']
@@ -24,10 +44,12 @@ class Dct:
              not a.startswith('_') and not callable(getattr(self, a))])
 
     def trigger(self, version):
-        self._render_files(version)
-        self._push_repo(version)
+        """Trigger tests by rendering file and pushing changes"""
+        self.render(version)
+        self.push(version)
 
-    def _render_files(self, version):
+    def render(self, version):
+        """only render the files for the trigger"""
         for src in LocalPath('tpl').list():
             tpl = Template(src.read(encoding='utf-8'))
             result = tpl.safe_substitute(
@@ -35,10 +57,11 @@ class Dct:
                 USER=self._devpiUser, DEVPI_INDEX=self._devpiIndex,
                 TIMESTAMP=datetime.now().ctime())
             dst = LocalPath(src.name)
-            log.debug("## rendered %s ##\n%s\n## -> %s ##\n", src, result, dst)
+            log.info("## rendered %s ##\n%s\n## -> %s ##\n", src, result, dst)
             dst.write(result, encoding='utf-8')
 
-    def _push_repo(self, version):
+    def push(self, version):
+        """only push the changes to origin"""
         git = local['git']
         git('add', '.')
         git('commit', '-m', '%s==%s' % (self._package, version))
@@ -47,4 +70,5 @@ class Dct:
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     fire.Fire(Dct())
