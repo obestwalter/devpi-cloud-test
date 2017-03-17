30784133004b06b85c56a48522feb58731e5a753
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-03-17 18:49:15 +0100

    add creation
    add function to create a new repo from a blueprint

--- blueprint/tpl/README.md
+++ blueprint/tpl/README.md
@@ -1,11 +1,11 @@
 # Results for [$PACKAGE==$VERSION]($DEVPI_INDEX/$PACKAGE/$VERSION)
 
-Cloud tests for [$PACKAGE](https://github.com/tox-dev/tox).
+Cloud tests for [$PACKAGE](FILL_IN_REPOSITORY_LINK).
 
 Test triggered at: $TIMESTAMP
 
-[![Build Status](https://travis-ci.org/obestwalter/devpi-cloud-test-tox.svg?branch=master)](https://travis-ci.org/obestwalter/devpi-cloud-test-tox)
+[![Build Status](FILL_ME_IN)](FILL_ME_IN)
 
-[![Build status](https://ci.appveyor.com/api/projects/status/98yyno2u5fpnds4l/branch/master?svg=true)](https://ci.appveyor.com/project/obestwalter/devpi-cloud-test-tox/branch/master)
+[![Build status](FILL_ME_IN)](FILL_ME_IN)
 
 This repo uses [devpi-cloud-test](https://github.com/obestwalter/devpi-cloud-test).
--- dct/cli.py
+++ dct/cli.py
@@ -9,16 +9,19 @@ from plumbum import local, LocalPath
 log = logging.getLogger(__name__)
 
 
-def with_sanity(func):
-    def _with_sanity(*args, **kwargs):
-        if not LocalPath(Dct._iniName).exists():
-            log.error("no %s in '%s'" % (Dct._iniName, local.cwd))
-            exit(1)
+class Config:
+    rel_ini_path = LocalPath('dct.ini')
 
-        return func(*args, **kwargs)
-    return _with_sanity
+    def __init__(self):
+        config = ConfigParser()
+        config.read(self.rel_ini_path)
+        dct = config['dct']
+        self.package = dct['package']
+        self.devpi_user = dct['devpi_user']
+        self.devpi_index = dct['devpi_index']
 
 
+# TODO Use `from plumbum.cli import Application` for cli
 class Dct:
     """Normal usage:
 
@@ -27,44 +30,57 @@ class Dct:
     Advanced usage see:
     https://github.com/google/python-fire/blob/master/doc/using-cli.md
     """
-    _iniName = 'dct.ini'
-
-    @with_sanity
-    def __init__(self):
-        config = ConfigParser()
-        config.read(self._iniName)
-        dct = config['dct']
-        self._package = dct['package']
-        self._devpiUser = dct['devpi_user']
-        self._devpiIndex = dct['devpi_index']
+    _tpl_path = LocalPath('tpl')
 
     def trigger(self, version):
         """Trigger tests by rendering file and pushing changes"""
-        self.render(version)
-        self.push(version)
+        self._render(version)
+        self._push(version)
 
-    def create(self, package):
-        for src in LocalPath('tpl').list():
-            tpl = Template(src.read(encoding='utf-8'))
+    def create(self, package, devpi_user='', devpi_index=''):
+        parent = local.cwd / ('devpi-cloud-test-' + package)
+        if parent.exists():
+            log.error("%s already exists - aborting", parent)
+            exit(1)
 
+        log.info("create new cloud test in %s", parent)
+        try:
+            blueprint_path = LocalPath(__file__).dirname.up() / 'blueprint'
+            blueprint_path.copy(parent)
+            with local.cwd(parent):
+                self.__render(Config.rel_ini_path,
+                              PACKAGE=package, DEVPI_USER=devpi_user,
+                              DEVPI_INDEX=devpi_index)
+        except Exception:
+            log.exception("creation failed")
+            parent.delete()
+            exit(1)
 
-    def render(self, version):
+        if not all((devpi_user, devpi_index)):
+            log.warning("not all options set, please add later in dct.ini")
+
+    def _render(self, version):
         """only render the files for the trigger."""
+        config = Config()
         for src in LocalPath('tpl').list():
-            tpl = Template(src.read(encoding='utf-8'))
-            result = tpl.safe_substitute(
-                PACKAGE=self._package, VERSION=version,
-                USER=self._devpiUser, DEVPI_INDEX=self._devpiIndex,
+            self.__render(
+                src, PACKAGE=config.package, VERSION=version,
+                DEVPI_USER=config.devpi_user, DEVPI_INDEX=config.devpi_index,
                 TIMESTAMP=datetime.now().ctime())
-            dst = LocalPath(src.name)
-            log.info("## rendered %s ##\n%s\n## -> %s ##\n", src, result, dst)
-            dst.write(result, encoding='utf-8')
 
-    def push(self, version):
+    @staticmethod
+    def __render(path, **kwargs):
+        tpl = Template(path.read(encoding='utf-8'))
+        result = tpl.safe_substitute(**kwargs)
+        dst = LocalPath(path.name)
+        log.info("## rendered %s ##\n%s\n## -> %s ##\n", path, result, dst)
+        dst.write(result, encoding='utf-8')
+
+    def _push(self, version):
         """only push the changes to origin"""
         git = local['git']
         git('add', '.')
-        git('commit', '-m', '%s==%s' % (self._package, version))
+        git('commit', '-m', '%s==%s' % (Config().package, version))
         git('push', 'origin', 'master')
         log.info("triggered test by pushing %s", local.cwd)
 
