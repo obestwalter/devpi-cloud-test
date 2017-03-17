5189e251deba02c57b6dee680f5f84ec40fe1f4a
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-03-17 18:48:09 +0100

    setup.py changes
    * use setuptools scm (just a stub, not using version yet)
    * more meta

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='devpi-cloud-test',
     author='Oliver Bestwalter',
+    url='https://github.com/obestwalter/devpi-cloud-test',
+    use_scm_version=True,
     description='CLI tool to manipulate cloud test repos and trigger tests',
-    install_requires=['fire', 'plumbum', 'devpy'],
-    entry_points={'console_scripts': ['dct = dct:main']},
+    setup_requires=['setuptools_scm'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['fire', 'plumbum', 'devpi'],
+    entry_points={'console_scripts': ['dct = dct.cli:main']},
+    license='MIT',
     classifiers=[
         'Intended Audience :: Developers',
         'Development Status :: 3 - Alpha',
